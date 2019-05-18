38507ffdb5c93a52da031bbe91306615ff9713a3
Drew McGowan <michael.mcgowan@students.makeschool.com>
2019-05-17 17:04:27 -0700

    code clean up

--- test.js
+++ test.js
@@ -8,11 +8,6 @@ const should = chai.should();
 const Ride = require('./models/ride');
 const User = require('./models/user');
 
-const sampleUser = {
-    username: 'username',
-    password: 'username',
-}
-
 const sampleRide =     {
     start: 'start',
     finish: 'finish',
@@ -54,70 +49,69 @@ describe('Ride', ()  => {
         });
     });
 
-    // TEST NEW
-    // it('should display new form on /rides/new GET', (done) => {
-    //     chai.request(server)
-    //     .get(`/rides/new`)
-    //     .end((err, res) => {
-    //         res.should.have.status(200);
-    //         res.should.be.html
-    //         done();
-    //     });
-    // });
-    // TEST SHOW
-    // it('should show a SINGLE ride on /rides/<id> GET', (done) => {
-    // const ride = new Ride(sampleRide);
-    // ride.save((err, data) => {
-    //     chai.request(server)
-    //     .get(`/rides/view/${ride._id}`)
-    //     .end((err, res) => {
-    //         res.should.have.status(200);
-    //         res.should.be.html
-    //         done();
-    //     });
-    // });
-    // });
-    // TEST EDIT
-    // it('should edit a SINGLE ride on /rides/<id>/edit GET', (done) => {
-    //     var ride = new Ride(sampleRide);
-    //     ride.save((err, data) => {
-    //         chai.request(server)
-    //         .get(`/rides/view/${ride._id}/edit`)
-    //         .end((err, res) => {
-    //             res.should.have.status(200);
-    //             res.should.be.html
-    //             done();
-    //         });
-    //     });
-    // });
-    // TEST CREATE
-    // it('should create a SINGLE ride on /rides POST', (done) => {
-    //     chai.request(server)
-    //     .post('/rides/view')
-    //     .send(sampleRide)
-    //     .end((err, res) => {
-    //         res.should.have.status(200);
-    //         res.should.be.html
-    //         done();
-    //     });
-    // });
-
-    // TEST UPDATE
-    // it('should update a SINGLE ride on /rides/<id> PUT', (done) => {
-    //     var ride = new Ride(sampleRide);
-    //     ride.save((err, data)  => {
-    //         chai.request(server)
-    //         .put(`/rides/view/${ride._id}?_method=PUT`)
-    //         .send({'title': 'Updating the title'})
-    //         .end((err, res) => {
-    //             res.should.have.status(200);
-    //             res.should.be.html
-    //             done();
-    //         });
-    //     });
-    // });
-    // Test Signup
-    it('should signup user', (done) => {
+  // TEST NEW
+  // it('should display new form on /rides/new GET', (done) => {
+  //     chai.request(server)
+  //     .get(`/rides/new`)
+  //     .end((err, res) => {
+  //         res.should.have.status(200);
+  //         res.should.be.html
+  //         done();
+  //     });
+  // });
+  // TEST SHOW
+  // it('should show a SINGLE ride on /rides/<id> GET', (done) => {
+  // const ride = new Ride(sampleRide);
+  // ride.save((err, data) => {
+  //     chai.request(server)
+  //     .get(`/rides/view/${ride._id}`)
+  //     .end((err, res) => {
+  //         res.should.have.status(200);
+  //         res.should.be.html
+  //         done();
+  //     });
+  // });
+  // });
+  // TEST EDIT
+  // it('should edit a SINGLE ride on /rides/<id>/edit GET', (done) => {
+  //     var ride = new Ride(sampleRide);
+  //     ride.save((err, data) => {
+  //         chai.request(server)
+  //         .get(`/rides/view/${ride._id}/edit`)
+  //         .end((err, res) => {
+  //             res.should.have.status(200);
+  //             res.should.be.html
+  //             done();
+  //         });
+  //     });
+  // });
+  // TEST CREATE
+  // it('should create a SINGLE ride on /rides POST', (done) => {
+  //     chai.request(server)
+  //     .post('/rides/view')
+  //     .send(sampleRide)
+  //     .end((err, res) => {
+  //         res.should.have.status(200);
+  //         res.should.be.html
+  //         done();
+  //     });
+  // });  
+  // TEST UPDATE
+  // it('should update a SINGLE ride on /rides/<id> PUT', (done) => {
+  //     var ride = new Ride(sampleRide);
+  //     ride.save((err, data)  => {
+  //         chai.request(server)
+  //         .put(`/rides/view/${ride._id}?_method=PUT`)
+  //         .send({'title': 'Updating the title'})
+  //         .end((err, res) => {
+  //             res.should.have.status(200);
+  //             res.should.be.html
+  //             done();
+  //         });
+  //     });
+  // });
+  // Test Signup
+  it('should signup user', (done) => {
     const user2 = {
       username: 'testtesttest',
       password: 'testestes',
